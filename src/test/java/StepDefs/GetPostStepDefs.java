188bf063691e8d1285380bf967238080652c57df
AzureAD\MarkBascara <mark@enhanceconsulting.co.nz>
2019-04-03 09:30:33 +1300

    First commit

--- src/test/java/StepDefs/GetPostStepDefs.java
+++ src/test/java/StepDefs/GetPostStepDefs.java
@@ -1,14 +1,8 @@
 package StepDefs;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
-import io.restassured.http.ContentType;
-
-import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
-import static org.hamcrest.CoreMatchers.is;
 
 public class GetPostStepDefs {
 
